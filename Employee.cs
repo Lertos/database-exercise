30915138e1cdd999a44b6581d54769db0e8057fa
Lertos <deetasticgaming@gmail.com>
2024-03-19 17:29:10 -0300

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_exercise
+{
+    internal class Employee
+    {
+        public string id { get; }
+        public string name { get; }
+        public string role { get; set; }
+        public double salary { get; private set; }
+
+        public Employee(string id, string name, string role, double salary = 0)
+        {
+            this.id = id;
+            this.name = name;
+            this.role = role;
+            this.salary = salary;
+        }
+
+        public override string? ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(id);
+            sb.Append(',');
+            sb.Append(name);
+            sb.Append(",");
+            sb.Append(role);
+            sb.Append(",");
+            sb.Append(salary);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+using database_exercise;
+
+Employee employee = new("1", "Jack Jackson", "Developer I", 70000.0);
+
+Console.WriteLine(employee.ToString());
