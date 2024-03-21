f543d28c868a55f7ac4a7a276de357e04359e7a1
Lertos <deetasticgaming@gmail.com>
2024-03-20 22:21:08 -0300

    Adding the DAO interface to be implemented

---
+++ EmployeeDOA.cs
@@ -0,0 +1,18 @@
+﻿namespace database_exercise
+{
+    internal interface EmployeeDOA
+    {
+        Employee GetEmployee(int id);
+
+        Employee[] GetEmployees();
+
+        int save(Employee employee);
+
+        int update(Employee employee);
+
+        int insert(Employee employee);
+
+        int delete(Employee employee);
+
+    }
+}
