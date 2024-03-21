c99dd46b0305a7eda60cfb84f71fe61ebdb3046a
Lertos <deetasticgaming@gmail.com>
2024-03-21 20:24:10 -0300

    Adding base logic for implementation

---
+++ EmployeeDAOImpl.cs
@@ -0,0 +1,32 @@
+﻿using dao;
+
+namespace database_exercise
+{
+    internal class EmployeeDAOImpl : EmployeeDAO
+    {
+        public int Delete(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
