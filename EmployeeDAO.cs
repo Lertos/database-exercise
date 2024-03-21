f957ae874c15a7f7c63ead1355843c4f6793bd5a
Lertos <deetasticgaming@gmail.com>
2024-03-20 22:30:56 -0300

    Extending the base DAO interface for Employee

---
+++ EmployeeDAO.cs
@@ -0,0 +1,8 @@
+﻿namespace dao
+{
+    //Used for implementing specific logic for Employees that don't fit in the base interface
+    internal interface EmployeeDAO : DAO<Employee>
+    {
+
+    }
+}
