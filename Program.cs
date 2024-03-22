e4f5c89dd9c07d7929b3e9f7a1196c94c5e8b3d6
Lertos <deetasticgaming@gmail.com>
2024-03-22 18:16:15 -0300

    Testing a simple get to check that the connection/user works

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 ﻿
 using dao;
+using Npgsql;
+using database_exercise;
 
 Employee employee = new(1, "Jack Jackson", "Developer I", 70000.0);
 
 Console.WriteLine(employee.ToString());
+
+await using NpgsqlConnection? conn = Database.GetConnection();
+
+if (conn == null)
+    return;
+
+await conn.OpenAsync();
+
+await using (var cmd = new NpgsqlCommand("SELECT * FROM employee", conn))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+        Console.WriteLine(reader.GetString(1));
+}
