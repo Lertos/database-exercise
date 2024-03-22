6fb63002c7dd194dfe2f54a0cd5aee68ed89d65a
Lertos <deetasticgaming@gmail.com>
2024-03-22 17:39:10 -0300

    Adding the base database class for connecting to the DB

---
+++ Database.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+
+namespace database_exercise
+{
+    internal class Database
+    {
+        private static string host = "";
+        private static string username = "";
+        private static string password = "";
+        private static string database = "";
+
+        private Database() { }
+
+        public static NpgsqlConnection GetConnection()
+        {
+            NpgsqlConnection connection = new NpgsqlConnection($"Host={host};Username={username};Password={password};Database={database};");
+
+            return connection;
+        }
+    }
+}
