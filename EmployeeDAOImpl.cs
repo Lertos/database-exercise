629805d488846376b45b77893c1bfdc07adcb166
Lertos <deetasticgaming@gmail.com>
2024-03-23 14:54:18 -0300

    Adding logic to get all employees from the database

--- EmployeeDAOImpl.cs
+++ EmployeeDAOImpl.cs
@@ -28,14 +28,62 @@ namespace database_exercise
             while (rdr.Read())
             {
                 Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2));
+                return null;
             }
-
-            return employee;
+            return null;
         }
 
-        public Employee[] GetAll()
+        public void Getd(int id)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection? conn = Database.GetConnection();
+            Employee? employee = null;
+
+            if (conn == null)
+                return;
+
+            conn.Open();
+
+            string sql = "SELECT * FROM employee";
+            using var cmd = new NpgsqlCommand(sql, conn);
+
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2));
+            }
+        }
+
+        public List<Employee> GetAll()
+        {
+            using NpgsqlConnection? conn = Database.GetConnection();
+
+            if (conn == null)
+                return [];
+
+            conn.Open();
+
+            string sql = "SELECT * FROM employee;";
+            using var cmd = new NpgsqlCommand(sql, conn);
+
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+            List<Employee> employees = new();
+
+            while (rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                string firstName = rdr.GetString(1);
+                string lastName = rdr.GetString(2);
+                string role = rdr.GetString(3);
+                double salary = rdr.GetDouble(4);
+
+                Employee employee = new(id, firstName, lastName, role, salary);
+
+                employees.Add(employee);
+            }
+
+            return employees;
         }
 
         public int Insert(Employee employee)
